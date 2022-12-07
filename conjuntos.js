cfdc7d558902281f2bf7413f19b841d37891eb86
Nicolas Origlia <noriglia@gmail.com>
2022-12-07 16:56:36 -0300

    guardando set de nombres

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+const arr = ['Agustina', 'Facundo', 'Gustavo', 'Gabriela', 'Isabella', 'Nicolas', 'Nicolas'];
+
+const setNames = new Set(arr);
+console.log(setNames);
+
+console.log(arr);
+setNames.add('Javascript');
+console.log(setNames);
